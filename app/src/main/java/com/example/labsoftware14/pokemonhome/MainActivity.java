198f4d32ac559beac2475ff49a95903fd220a525
kevinvegah <curosaquiflow12>
2017-09-11 16:27:35 -0500

    pokemonhome

--- app/src/main/java/com/example/labsoftware14/pokemonhome/MainActivity.java
+++ app/src/main/java/com/example/labsoftware14/pokemonhome/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.labsoftware14.pokemonhome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         final TextView mTextView = (TextView) findViewById(R.id.text);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
 
     }
+
+
 }
